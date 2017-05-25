e67e728e10d0f1109affc8138aed5eeddec8c9a9
Chris Hitchcott <hitchcott@gmail.com>
2017-05-25 23:31:37 +0100

    [maint] redeploy

--- scripts/populate.js
+++ scripts/populate.js
@@ -1,19 +1,19 @@
 // Usage in Truffle:
-// const data = require('/Users/chris/code/digix/etc-refund/scripts/data/balances-3765660-1495722676586.json');
-// const p = require('./scripts/populate.js');
-// p(FaucetRegistry,data);
+/*
+const data = require('/Users/chris/code/digix/etc-refund/scripts/data/balances-3765660-1495722676586.json');
+const p = require('./scripts/populate.js');
+p(web3, FaucetRegistry, data);
+*/
 
 const a = require('awaiting');
 
-const maxDgd = 8600300;
 const fundAmount = 2000000000000000;
 
 module.exports = async (Contract, data) => {
   const contract = Contract.at(Contract.address);
   const { balances } = data;
-  const keys = Object.keys(balances).filter(key => balances[key].combined < maxDgd);
+  const keys = Object.keys(balances);
   console.log(`Populating ${keys.length} balances...`);
-  // TODO don't give to people with more than the minimum requirement
   let i = 0;
   await a.map(keys, 10, (key) => {
     i += 1;
